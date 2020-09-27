9f91c5ad8c4b2416bbf4b7a69314904e3f54fa18
nischalshrestha <bitsorific@gmail.com>
2020-09-26 22:41:59 -0400

    test case based result grading is working

--- pygradethis/result_checker.py
+++ pygradethis/result_checker.py
@@ -1,58 +1,91 @@
+# core
 import sys
 from io import StringIO
 from itertools import zip_longest
+from collections import namedtuple
+from typing import Any, Tuple
 
-from pygradethis.conditions import python_pass_if, python_fail_if
-
+from .conditions import GraderCondition
+# testing
 import unittest
 from pandas.testing import assert_frame_equal, assert_series_equal
 
 class TestCondition(unittest.TestCase):
-
-    def __init__(self, test_name, condition):
+    """Custom TestCase to do asserts on GraderCondition(s) and incorporate
+    custom asserts from libraries like pandas for dataframe checking.
+    """
+    def __init__(self, test_name: str, condition: GraderCondition):
         super(TestCondition, self).__init__(test_name)
         self.condition = condition
         
     ## Test Case for `GraderCondition`
     def test_condition(self):
-        actual, expected = self.condition
+        ID, actual, expected = self.condition
         try:
             if type(actual).__name__ == "DataFrame" and type(expected).__name__ == "DataFrame":
                 self.assertTrue(assert_frame_equal(actual, expected))
             self.assertEqual(actual, expected)
         except Exception:
-            self.fail()
+            # Note: this ID isn't really being used at the moment for error
+            # checking
+            self.fail(ID)
 
-if __name__ == '__main__':
-    sys.stderr = StringIO()     # capture output
+# we want custom result for `TextTestRunner`
+class ConditionTestResult(unittest.TextTestResult):
+    """Custom test result where we maintained a matched test ID list to reference
+    later when determining matched conditions.
+    """
+    def __init__(self, stream, descriptions: bool, verbosity: int):
+        super(ConditionTestResult, self).__init__(stream, descriptions, verbosity)
+        self.matched = []
+
+    def addSuccess(self, test: TestCondition):
+        super(ConditionTestResult, self).addSuccess(test)
+        # add matched case
+        self.matched.append(test.condition.id)
+
+def test_conditions(*conditions: GraderCondition, 
+                    user_result: Any = None, 
+                    default_correct: bool = False,
+                    deafult_message: str = "",
+                    r: dict = {}) -> Tuple[bool, GraderCondition]:
     # create a test suite
     suite = unittest.TestSuite()
-
-    # TODO try to make TestCondition based on GradeCondition(s)
-    conditions = [
-        python_pass_if("1", "1 is correct!"),
-        python_pass_if(2, "2 is correct!"),
-        python_fail_if(1, "1 is incorrect!"),
-        python_fail_if("3", "3 is incorrect!"),
-    ]
-    cases = [
-        TestCondition('test_condition', ("1", "1")),
-        TestCondition('test_condition', (2, 2)),
-        TestCondition('test_condition', (2, 2)),
-        TestCondition('test_condition', ("3", "3"))
-    ]
-    # add test cases
-    for c in cases:
-        suite.addTest(c)
+    # convenience tuple for structuring test cases
+    TestCase = namedtuple("TestCase", ["id", "actual", "expected"])
+    # make TestCondition based on GradeCondition(s)
+    for i, c in enumerate(conditions):
+        suite.addTest(
+            TestCondition(
+                'test_condition', TestCase(i, user_result, c['x'])
+            )
+        )
+    # capture current sys.stderr buffer (default one)
+    default_buffer = sys.stderr
+    # capture test print output for AssertionError(s)
+    sys.stderr = StringIO()
     # run suite
-    results = unittest.TextTestRunner().run(suite)
-    print("# unmatched conditions: {}".format(len(results.failures)))
-
-    # TODO matching logic can be incorporated here
-    for i, (case, failure) in enumerate(zip_longest(cases, results.failures, fillvalue="")):
-        # print(type(case), type(failure))
-        if failure == "":
-            print("{} matched!".format(conditions[i]))
-        else:
-            print("{} didn't match!".format(conditions[i]))
-
+    results = unittest.TextTestRunner(resultclass = ConditionTestResult).run(suite)
+    # restore stderr to buffer so we catch any errors on our matching code below
+    sys.stderr = default_buffer
+    
+    # get matched conditions by subsetting conditions list with matched index list
+    matched_conditions = list(map(lambda i: conditions[i], results.matched))
+    incorrect_match = None
+    if len(matched_conditions) > 0:
+        # return on the first `python_pass_if`
+        for mc in matched_conditions:
+            if mc['correct']:
+                return True, mc
+            elif incorrect_match == None:
+                # keep record of the first `python_fail_if` to return
+                incorrect_match = mc
+    else: # no match
+        # - if there is at least one pass_if() condition, then default to an incorrect grade 
+        # - otherwise, we default to a correct grade
+        if len([c for c in conditions if c['correct']]) != 0:
+            return default_correct, None
+        # otherwise if we only have fail_ifs, we default to a correct grade 
+        elif len([c for c in conditions if not c['correct']]) != 0:
+            return True, None
+    return default_correct, incorrect_match
