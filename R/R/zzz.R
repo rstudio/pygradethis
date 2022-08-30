083ebd93b1646bc8ddaeb44416b9a019ae796c9c
Garrick Aden-Buie <garrick@adenbuie.com>
2022-08-30 12:08:04 -0400

    Safely initialize python session in {pygradethis} (#53)

--- R/R/zzz.R
+++ R/R/zzz.R
@@ -1,3 +1,10 @@
+inform_python_pygradethis_not_found <- function(..., throw = stop) {
+  throw(
+    "Python or the Python pygradethis package were not found when {pygradethis} was loaded. ",
+    "Please verify your pygradethis or Python installation and restart your R session."
+  )
+}
+
 #' The main `pygradethis` module that can be used to access other
 #' submodules and functions within the Python library.
 #'
@@ -9,17 +16,29 @@ pygradethis <- NULL
 #'
 #' @return the `pygradethis_exercise_checker` Python function
 #' @export
-pygradethis_exercise_checker <- NULL
+pygradethis_exercise_checker <- inform_python_pygradethis_not_found
 
 #' Get the last value of Python source code
 #'
 #' @return the value
 #' @export
-get_last_value <- NULL
+get_last_value <- inform_python_pygradethis_not_found
+
 
 .onLoad <- function(libname, pkgname) {
   # import `pygradethis` and the exercise checking function
-  pygradethis <<- reticulate::import("pygradethis", convert=FALSE, delay_load = TRUE)
-  pygradethis_exercise_checker <<- pygradethis$pygradethis_exercise_checker$pygradethis_exercise_checker
-  get_last_value <<- pygradethis$utils$get_last_value
+  if (reticulate::py_available(initialize = TRUE)) {
+    tryCatch({
+      pygradethis <<- reticulate::import("pygradethis", convert=FALSE, delay_load = TRUE)
+      pygradethis_exercise_checker <<- pygradethis$pygradethis_exercise_checker$pygradethis_exercise_checker
+      get_last_value <<- pygradethis$utils$get_last_value
+    }, error = function(err) {
+      message(
+        "An error occurred while trying to connect {pygradethis} in R to the Python companion package:\n",
+        conditionMessage(err)
+      )
+    })
+  } else {
+    inform_python_pygradethis_not_found(throw = message)
+  }
 }
